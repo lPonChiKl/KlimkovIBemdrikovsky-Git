3d1f8430c0865c38e623381df00d88ff02d45946
Alexey <klimkov.1998@mail.ru>
2021-09-09 14:13:44 +0300

    Им1 Добавление строки привет мир а русском

--- KlimkovIBemdrikovsky Git/Program.cs
+++ KlimkovIBemdrikovsky Git/Program.cs
@@ -11,6 +11,7 @@ namespace KlimkovIBemdrikovsky_Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
             Console.WriteLine("Привет мир!");
         }
     }
